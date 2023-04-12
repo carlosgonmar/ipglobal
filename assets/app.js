a48304706dc9fdabe219ca6440884ee1ece447a4
Carlos Gonzalez <contacto@carlosgonzalezmartin.com>
2023-04-12 15:03:46 +0000

    Add SassLoader

---
+++ assets/app.js
@@ -0,0 +1,17 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './css/bootstrap/bootstrap.min.css';
+import './css/base.css';
+import './css/error.css';
+
+// start the Stimulus application
+import './js/jquery-3.2.1.slim.min.js';
+import './js/popper.min.js';
+import './js/holder.min.js';
+import './bootstrap';
